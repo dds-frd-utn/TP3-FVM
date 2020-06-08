24f3172cfe9b9ca9cc916fba07f36d45ba3822e1
Federico <toledofederico11@gmail.com>
2020-06-07 22:27:40 -0300

    initial commit

---
+++ app/src/main/java/utn/frd/fvm/MainActivity.java
@@ -0,0 +1,21 @@
+package utn.frd.fvm;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+}
