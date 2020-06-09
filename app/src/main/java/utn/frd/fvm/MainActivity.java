99e3afed28dac7780d65b2453f4647ada9740dc6
mateo <mateo.ryser@gmail.com>
2020-06-09 15:55:57 -0300

    Linkeado grafica logica de cuentas

--- app/src/main/java/utn/frd/fvm/MainActivity.java
+++ app/src/main/java/utn/frd/fvm/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btTuPerfil;
+    private Button btMovimientos;
+    private Button btRealizarPago;
+    private Button btCuentas;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(mainToolbar);
+
+        btCuentas = findViewById(R.id.btCuentas);
+        btTuPerfil = findViewById(R.id.btTuPerfil);
+        btMovimientos = findViewById(R.id.btMovimientos);
+        btRealizarPago = findViewById(R.id.btRealizarPago);
+
+        btCuentas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irACuentas();
+            }
+        });
     }
+    void irACuentas()
+    {
+        Intent i = new Intent(this,Cuentas.class);
+        startActivity(i);
+
+    }
+
+
 }
