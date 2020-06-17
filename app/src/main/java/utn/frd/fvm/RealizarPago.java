a0d4e5e79fd750becc2333c3fd583b25592bf0f0
Federico <toledofederico11@gmail.com>
2020-06-17 15:01:40 -0300

    funcionalidad realizar pago terminada

--- app/src/main/java/utn/frd/fvm/RealizarPago.java
+++ app/src/main/java/utn/frd/fvm/RealizarPago.java
@@ -1,20 +1,218 @@
 package utn.frd.fvm;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class RealizarPago extends AppCompatActivity {
+
+    private Spinner inputCuentaOrigen;
+    private EditText inputCuentaDestino;
+    private EditText inputCantidad;
+    private Button btnEnviarDinero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realizar_pago);
 
+        //Obtengo el id del cliente
+        final Integer idCliente = getIntent().getIntExtra("ID",0);
+
         Toolbar mainToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(mainToolbar);
 
+        inputCuentaOrigen = findViewById(R.id.inputCuentaOrigen);
+        inputCuentaDestino = findViewById(R.id.inputCuentaDestino);
+        inputCantidad = findViewById(R.id.inputCantidad);
+        btnEnviarDinero = findViewById(R.id.btnEnviarDinero);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        new GetCuentasCliente(idCliente, this).execute();
+
+        btnEnviarDinero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ORIGEN", inputCuentaOrigen.getSelectedItem().toString());
+                new VerificarSaldo(inputCuentaOrigen.getSelectedItem().toString()).execute();
+            }
+        });
+
     }
+
+    private class GetCuentasCliente extends AsyncTask<String, String, String> {
+        private Integer id;
+        private Context context;
+
+        private GetCuentasCliente(Integer id, Context context) {
+            this.id = id;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result;
+            try {
+                result = RESTService.makeGetRequest("http://192.168.100.6:8080/TP1-FVM/rest/cuentas/clientes/"+this.id);
+                return result;
+            } catch(Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray resJson = new JSONArray(result);
+                List<String> listaAlias = new ArrayList<>();
+                ArrayAdapter<String> adapter;
+                listaAlias.add("Seleccione una cuenta");
+                for(int i=0; i < resJson.length(); i++) {
+                    Log.d("LLAVE",resJson.getJSONObject(i).getString("aliasCuenta"));
+                    listaAlias.add(resJson.getJSONObject(i).getString("aliasCuenta"));
+                }
+                adapter = new ArrayAdapter<String>(this.context,R.layout.spinner_item,listaAlias){
+                    @Override
+                    public boolean isEnabled(int position) {
+                        if(position == 0){
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+
+                    @Override
+                    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                        View view = super.getDropDownView(position, convertView, parent);
+                        TextView tv = (TextView) view;
+                        if(position == 0) {
+                            tv.setTextColor(Color.GRAY);
+                        } else {
+                            tv.setTextColor(Color.BLACK);
+                        }
+                        return view;
+                    }
+                };
+                adapter.setDropDownViewResource(R.layout.spinner_item);
+                inputCuentaOrigen.setAdapter(adapter);
+                inputCuentaOrigen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        String selectedItem = (String) parent.getItemAtPosition(position);
+                        if(position > 0) {
+                            Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class EnviarDinero extends AsyncTask<String, String, String> {
+        private JSONObject transaccion;
+
+        private EnviarDinero(JSONObject transaccion) {
+            this.transaccion = transaccion;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result;
+            try {
+                result = RESTService.restCall("http://192.168.100.6:8080/TP1-FVM/rest/transacciones/realizar", "POST", transaccion);
+            } catch(Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Ocurrio un error, intente mas tarde",Toast.LENGTH_SHORT).show();
+                return null;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getApplicationContext(),s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class VerificarSaldo extends AsyncTask<String, String, String> {
+        private String alias;
+
+        private VerificarSaldo(String alias) {
+            this.alias = alias;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result;
+            try {
+                result = RESTService.makeGetRequest("http://192.168.100.6:8080/TP1-FVM/rest/cuentas/alias/"+this.alias);
+            } catch(Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject resJson = new JSONObject(result);
+                JSONObject transaccion = new JSONObject();
+                transaccion.put("cuentaOrigen", resJson.getInt("id"))
+                           .put("cuentaDestino", inputCuentaDestino.getText().toString())
+                           .put("monto", Integer.parseInt(inputCantidad.getText().toString()))
+                           .put("tipoTransaccion", 0) //TODO hay que ver que tipo es
+                           .put("fecha", new Date());
+
+                int saldo = resJson.getInt("saldo");
+                int monto = transaccion.getInt("monto");
+
+                if(saldo > monto) {
+                    new EnviarDinero(transaccion).execute();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Saldo insuficiente", Toast.LENGTH_SHORT).show();
+                    inputCantidad.setText("");
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
